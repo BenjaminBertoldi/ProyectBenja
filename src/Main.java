c41da1c8cc11b5a699bfd1e68ed2d24d7c4c4de0
Benja <benjaminbertoldi23@gmail.com>
2023-05-25 22:31:00 -0300

    hola

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
         Uimenu.showMenu();
+        System.out.println("hola");
     }
 }
